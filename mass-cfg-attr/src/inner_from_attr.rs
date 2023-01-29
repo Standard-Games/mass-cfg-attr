9f19020b629eac6e332c668152518f41d0379487
Daniel Mason <daniel@danielmason.com>
2023-01-29 14:23:43 +0000

    remove let ... else so macro works on older versions of Rust

--- mass-cfg-attr/src/inner_from_attr.rs
+++ mass-cfg-attr/src/inner_from_attr.rs
@@ -4,12 +4,15 @@ use quote::ToTokens;
 use venial::Attribute;
 
 pub fn inner_from_attr(attr: &Attribute) -> TokenStream {
-    let Some(group) = attr.to_token_stream().into_iter().filter_map(|tt| match tt {
-        TokenTree::Group(group) => Some(group),
-        _ => None,
-    }).next() else {
-        panic!("This should not be possible");
-    };
+    let group = attr
+        .to_token_stream()
+        .into_iter()
+        .filter_map(|tt| match tt {
+            TokenTree::Group(group) => Some(group),
+            _ => None,
+        })
+        .next()
+        .expect("This should not be possible, at this point, there should always be an inner part");
     group.stream()
 }
 
@@ -22,10 +25,10 @@ mod tests {
 
     fn get_attr_str(attr_stt: &str) -> Attribute {
         let ts = TokenStream::from_str(&format!("{}\nfn fun(){}", attr_stt, "{}")).unwrap();
-        let Ok(Declaration::Function(fun)) = venial::parse_declaration(ts) else {
-            panic!("This should not be possible")
-        };
-        fun.attributes[0].clone()
+        match venial::parse_declaration(ts) {
+            Ok(Declaration::Function(fun)) => fun.attributes[0].clone(),
+            _ => panic!("Test unexpectedly failed to parse token stream"),
+        }
     }
 
     #[test]
